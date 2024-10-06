67537d414475b162869040bd35119c2863f9254f
foisal <foisalrk2@gmail.com>
2024-10-06 18:31:08 +0600

    pooo

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("ecommerceWebsite");
     const menuCollection = database.collection("menu");
     const cartCollection = database.collection("carts");
