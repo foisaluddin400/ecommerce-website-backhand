c6a3528a5e1ae04a4a9c34b4706412b56fa901ad
foisal <foisalrk2@gmail.com>
2024-10-06 16:43:34 +0600

    eccomerce

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("ecommerceWebsite");
     const menuCollection = database.collection("menu");
     const cartCollection = database.collection("carts");
@@ -260,8 +260,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close(); // Uncomment this if you want to close the client after running
   }
